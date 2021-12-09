0e4074b95b29b44fb89a8c137f96d438a1bf1476
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2021-12-09 14:25:48 +0500

    Section 4 - Navigation between Screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,37 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreenn</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreenn</Text>
+      <Button
+        onPress={() => {
+          // console.log("Button Pressed");
+          navigation.navigate("Components");
+        }}
+        title="Go to Components Demo"
+      />
+
+      <Button
+        onPress={() => {
+          // console.log("Button Pressed");
+          navigation.navigate("List");
+        }}
+        title="Go to List Demo"
+      />
+      {/* Touchable Opacity */}
+
+      {/* <TouchableOpacity
+        onPress={() => {
+          // console.log("List Button Pressed");
+          props.navigation.navigate("List");
+        }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
